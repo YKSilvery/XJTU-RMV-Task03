ba9c3ff58f5d9afba6fb046e17491e2d33f63ee9
SilverySKY <yksilvery@gmail.com>
2025-09-25 20:34:02 +0800

    Add files via upload

---
+++ build/CMakeFiles/ceres_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ceres_app.dir/src/main.cpp.o"
+  "CMakeFiles/ceres_app.dir/src/main.cpp.o.d"
+  "ceres_app"
+  "ceres_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ceres_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
